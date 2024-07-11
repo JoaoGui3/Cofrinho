205e9d94fac095c557489f84616d4f2ddecde9ce
JoaoGui3 <joaoguilherme36210.1@gmail.com>
2024-07-11 15:46:00 -0300

    upando parojeto

--- src/Euro.java
+++ src/Euro.java
@@ -1,2 +1,11 @@
-public class Euro {
+class Euro extends Moeda {
+    public Euro(double valor) {
+        super(valor, "Europa");
+    }
+
+    @Override
+    public double getValorEmReal() {
+        // Implemente a conversão para Real aqui (exemplo: 1 euro = 6,20 reais)
+        return getValor() * 6.20;
+    }
 }
